5e8398b4e03ca8830bc26559858a9ae0df1dabe3
CALLUM BURNS <CBU787@GMAIL.COM>
2016-03-22 14:08:48 -0400

    Completed Exercise 1

---
+++ exercise1.rb
@@ -0,0 +1,18 @@
+puts 2 + 3
+
+puts 2
+puts 3
+puts 2 + 3
+
+puts "Hello World"
+puts 'Hello World'
+
+"Betty's pie shop"
+'Betty\s pie shop'
+
+puts "Ada Lovelace lived for #{1852 - 1815} years."
+
+puts "Hello\t\tWorld"
+puts "Hello\b\b\b\b\bWorld"
+puts "Hello\rStart over world"
+puts "1.Hello\n2. World"
