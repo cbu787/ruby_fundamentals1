76d8d2af695ee97934d33272a1262aa3b63a0b2e
CALLUM BURNS <CBU787@GMAIL.COM>
2016-03-22 15:27:41 -0400

    Completed Exercise 4.

--- exercise4.rb
+++ exercise4.rb
@@ -1,5 +1,17 @@
 range = 1..100
 
+
 range.each do |num|
-puts num
+  divides_by_3 = ( num % 3 == 0)
+  divides_by_5 = ( num % 5 == 0)
+  case
+    when divides_by_3 && divides_by_5
+    puts "Bitmaker"
+    when divides_by_3
+      puts "Bit"
+    when divides_by_5
+      puts "Maker"
+    else puts num
+  end
+puts
 end
